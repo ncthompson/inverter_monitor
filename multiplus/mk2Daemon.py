6656c1aa0fee556622938a4bdbd2c1f1000eaeae
NC Thompson <github@hoekwil.com>
2015-01-12 22:49:05 +0200

    Updated mpStore and integrated in to mk2Daemon
    Busy implementing multi threading on mk2Daemon...still broken.

--- multiplus/mk2Daemon.py
+++ multiplus/mk2Daemon.py
@@ -3,28 +3,54 @@
 '''
 Copyright (c) 2015, NC Thompson
 All rights reserved.
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
 
-1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+1) Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
 
-2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+2) Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation 
+    and/or other materials provided with the distribution.
 
-3) Neither the name of the ORGANIZATION nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+3) Neither the name of the ORGANIZATION nor the names of its contributors may 
+    be used to endorse or promote products derived from this software without 
+    specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 
 import serial
 import time
+import sys
+import signal
 from usbid.device import device_list
+from mpStore import Mk2Store
+from threading import Thread, Lock, Event
 
 class Mk2:
-    def __init__(self):
+    def __init__(self, stop_event):
+        self.device = Mk2Store()
         self.ser = serial.Serial()
         self.ser.baudrate = 2400
         self.ser.port = '/dev/'+self.getTtyDevice()
         self.ser.open()
         self.serOpen = True
+        self.stop_event = stop_event
+        
+        self.lock = Lock()
+        #self.mk2Run = True
+
+    #def stopDaemon(self):
+    #    self.mk2Run = False
 
     def printLed(self, binStr):
         names = ["temperature", "low_battery", "overload", "inverter", "float", "bulk", "absorption", "mains"]
@@ -93,20 +119,31 @@ class Mk2:
     def ledDecode(self,frame):
         on = '{0:08b}'.format(frame[0])
         blink = '{0:08b}'.format(frame[1])
-        print "LEDs On: "
-        self.printLed(on)
-        print "LEDs Blink: "
-        self.printLed(blink)
+        #print "LEDs On: "
+        #self.printLed(on)
+        #print "LEDs Blink: "
+        #self.printLed(blink)
+
+        self.device.printState()
         self.ser.close()
         self.serOpen = False
-        time.sleep(10)
+        self.mk2Run = False
+        self.stop_event.wait(10)
+        #time.sleep(10)
         print "\n-----------------------------------------------------------------------------"
         
     def dcDecode(self,frame):
-        print "Voltage: ", round((frame[5] + frame[6]*256)/100.0, 2)
-        print "Used Current: ", round((frame[7] + frame[8]*256 + frame[9]*256*256)/100.0, 2)
-        print "Charging Current: ", round((frame[10] + frame[11]*256 + frame[12]*256*256)/100.0, 2)
-        print "Inverter Period: ", round(100000.0/frame[13], 1)
+        batVoltage = round((frame[5] + frame[6]*256)/100.0, 2)
+        usedCurrent = round((frame[7] + frame[8]*256 + frame[9]*256*256)/100.0, 2)
+        chargingCurrent = round((frame[10] + frame[11]*256 + frame[12]*256*256)/100.0, 2)
+        inverterFreq = round(100000.0/frame[13], 1)
+        
+        batCurrent = usedCurrent - chargingCurrent
+        self.lock.acquire()
+        self.device.setBatVoltage(batVoltage)
+        self.device.setBatCurrent(batCurrent)
+        self.device.setOutFreq(inverterFreq)
+        self.lock.release()
     
         # L1
         send = self.frameCommand(['F', 1])
@@ -115,12 +152,19 @@ class Mk2:
     def acDecode(self,frame):
         #print "BF Factor: ", (frame[0])/128.0-1
         #print "Inv Factor: ", (frame[1])/128.0-1
-        print "Mains Voltage: " , round((frame[5] + frame[6]*256)/100.0, 1)
-        print "Mains Current: " , round((frame[7] + frame[8]*256)/100.0, 2)
-        print "Inv Voltage: " , round((frame[9] + frame[10]*256)/100.0, 1)
-        print "Inv Current: " , round((frame[11] + frame[12]*256)/100.0, 2)
-        print "Mains Period: ", round((10000.0/frame[13]), 1)
+        inVoltage = round((frame[5] + frame[6]*256)/100.0, 1)
+        inCurrent = round((frame[7] + frame[8]*256)/100.0, 2)
+        outVoltage =  round((frame[9] + frame[10]*256)/100.0, 1)
+        outCurrent = round((frame[11] + frame[12]*256)/100.0, 2)
+        inFreq = round((10000.0/frame[13]), 1)
         
+        self.lock.acquire()
+        self.device.setInVoltage(inVoltage)
+        self.device.setInCurrent(inCurrent)
+        self.device.setOutVoltage(outVoltage)
+        self.device.setOutCurrent(outCurrent)
+        self.device.setInFreq(inFreq)
+        self.lock.release()
         # LED
         send = self.frameCommand(['L'])
         self.sendFrame(send)
@@ -128,7 +172,7 @@ class Mk2:
     
     def servMulti(self):
         dat = ""
-        while True:
+        while not self.stop_event.is_set():
             if self.serOpen == False:
                 self.ser.open()
                 self.serOpen = True
@@ -142,7 +186,22 @@ class Mk2:
                     frame.append(temp)
                 #print frame
                 self.deframe(frame)
-
+    
+    #def run(self):
+    
 if __name__ == "__main__":
-    mk = Mk2()
-    mk.servMulti()
+    mk2Stop = Event()
+    mk2 = Mk2(mk2Stop)
+    try:
+        print "1"
+        usbDaemon = Thread(target = mk2.servMulti)
+        print "2"
+        #usbDaemon.start()
+        print "3"
+        #usbDaemon.join()
+        print "4"
+    except KeyboardInterrupt:
+        print "Stopping Daemon"
+        mk2Stop.set()
+    #mk.servMulti()
+    
--- multiplus/mpStore.py
+++ multiplus/mpStore.py
@@ -1,16 +1,31 @@
 '''
 Copyright (c) 2015, NC Thompson
 All rights reserved.
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
 
-1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+1) Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
 
-2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+2) Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation 
+    and/or other materials provided with the distribution.
 
-3) Neither the name of the ORGANIZATION nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+3) Neither the name of the ORGANIZATION nor the names of its contributors may 
+    be used to endorse or promote products derived from this software without 
+    specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
+
 class Mk2Store:
     
     def __init__(self):
@@ -27,7 +42,13 @@ class Mk2Store:
         self.outCurrent = 0
         self.outFreq = 0
 
-    def setBatVoltage(self. voltage):
+        # Save LED state
+        self.temperature_state = 0
+        self.power_out_state = 0
+        self.power_in_state = 0
+        self.battery_state = 0
+
+    def setBatVoltage(self, voltage):
         self.batVoltage = voltage
 
     def setBatCurrent(self, current):
@@ -50,3 +71,27 @@ class Mk2Store:
 
     def setOutFreq(self, freq):
         self.outFreq = freq
+    
+    def setTempState(self, state):
+        self.temperature_state = state
+
+    def setPowerOutState(self, state):
+        self.power_out_state = state
+
+    def setPowerInState(self, state):
+        self.power_in_state = state
+
+    def setBatteryState(self, state):
+        self.battery_state = state
+    
+    def printState(self):
+        print "Battery Voltage: ", self.batVoltage
+        print "Battery Current: ", self.batCurrent
+
+        print "In Voltage: ", self.inVoltage
+        print "In Current: ",  self.inCurrent
+        print "In Frequency: ", self.inFreq
+
+        print "Out Voltage: ", self.outVoltage
+        print "Out Current: ", self.outCurrent
+        print "Out Frequency: ", self.outFreq
